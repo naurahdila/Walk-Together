67ea9120148c6e10028c3e632f8064e47c9cab74
dapaputra1 <fsdaffa610@gmail.com>
2024-12-18 15:23:11 +0700

    terbaru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -177,4 +177,4 @@
 
 
 
-<h1>HALLO</h1> --}}
+<h1>HALLO</h1>
